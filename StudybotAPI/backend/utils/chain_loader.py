30f7c4329062c14614d8d6eb83792d92982fab25
HemanthSai7 <hemanth.sai3187@gmail.com>
2023-12-19 16:12:10 +0530

    qa chain modifications

--- StudybotAPI/backend/utils/chain_loader.py
+++ StudybotAPI/backend/utils/chain_loader.py
@@ -21,7 +21,7 @@ async def llm_chain_loader(DATA_PATH: str):
         prompt = f.read()
 
     prompt = PromptTemplate(
-        template=prompt, input_variables=["context", "chat_history", "question"]
+        template=prompt, input_variables=["context", "question"]
     )
 
     llm = Clarifai(
@@ -32,24 +32,26 @@ async def llm_chain_loader(DATA_PATH: str):
         model_version_id=config.MODEL_VERSION_ID,
     )
 
-    # qa_chain = RetrievalQA.from_chain_type(
-    #     llm=llm,
-    #     chain_type="stuff",
-    #     retriever=db.as_retriever(search_type="similarity",search_kwargs={"k": 2}),
-    #     return_source_documents=True,
-    #     chain_type_kwargs={"prompt": prompt},
-    # )
-
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    qa_chain = ConversationalRetrievalChain.from_llm(
+    qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         retriever=db.as_retriever(
-            search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4}),
-        # return_source_documents=True,
-        # chain_type_kwargs={"prompt": prompt},
-        condense_question_prompt=prompt,
-        memory=memory,
+            search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4}
+        ),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
     )
+
+    # memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # qa_chain = ConversationalRetrievalChain.from_llm(
+    #     llm=llm,
+    #     chain_type="stuff",
+    #     retriever=db.as_retriever(
+    #         search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4}),
+    #     # return_source_documents=True,
+    #     # chain_type_kwargs={"prompt": prompt},
+    #     condense_question_prompt=prompt,
+    #     memory=memory,
+    # )
 
     app.state.qa_chain = qa_chain
